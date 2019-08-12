cdef45b0f170e83d182d08c378932a604811cc9a
Shasha <46234388+sha256feng@users.noreply.github.com>
2019-08-12 18:00:26 -0400

    Update step1_run.sh

--- assemble-system/Model-long-missing-loop/modeller-model/automate_script/step1_run.sh
+++ assemble-system/Model-long-missing-loop/modeller-model/automate_script/step1_run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#Input: step1_preprocess.tcl, PDB file
 
-#Truncate unnecessary residues
+#Truncate unnecessary residues and write PDB file for each chain;
 vmd -dispdev text -eofexit < step1_preprocess.tcl > step1_preprocess.log
